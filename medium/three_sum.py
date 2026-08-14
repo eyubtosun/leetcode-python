dec28f08d9f205f5ee1b5f6b107eed8ef2c1a49b
Eyub Tosun <eyubtosun@users.noreply.github.com>
2026-08-13 21:33:48 -0500

    Complete Three Sum solution

--- medium/three_sum.py
+++ medium/three_sum.py
@@ -4,3 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
+        triplets = []
+
+        for index in range(len(nums) - 2):
+            if index > 0 and nums[index] == nums[index - 1]:
+                continue
+
+            left = index + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[index] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    triplets.append([nums[index], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return triplets
